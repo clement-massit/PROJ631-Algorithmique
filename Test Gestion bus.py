b87a514e2ec7f09b86307d9df552d1b6c12ef6bc
clement-massit <clement.massit@gmail.com>
2021-02-09 11:42:49 +0100

    Update Test Gestion bus.py

--- Test Gestion bus.py
+++ Test Gestion bus.py
@@ -7,36 +7,88 @@ Ceci est un script temporaire.
 
 #Enoncé : Gestion d'un réseau de bus
 
-from data2py import data2dic
+from data2py import dates2dic
 
+
+#=================================================
+#lignes de bus: 
 data_file_name = '1_Poisy-ParcDesGlaisins.txt'
+
+
 try:
     with open(data_file_name, 'r') as f:
-        content = f.read()
-    
+        content = f.read()   
 except OSError:
     print('file not found')
     
-#Classe Arret a un label    
+#=================================================   
+#Classe Arret    
 class Arret:
-    def __init__(self,label):
+    def __init__(self,label,liste_horaire_linge):
         self.label = label
+        self.liste_horaire_ligne
         
     
     def get_label(self):
         return self.label
 
-    def get_liste_horaire(self):
-        regular_date_go = dates2dic(content.plit('\n\n\n'))
-        return regular_date_go[self]
+    def time_bw_arret(self,heure):
+        
+
 
  
-print('Vernod'.get_liste_horaire())
-       
 class Bus:
     def __init__(self,label,start,end):
         self.label = label
         self.start = start
         self.end = end
         
+        
+        
+        
+
+#=================================================
+#methode general pour recuperer la ligne de bus sous forme d'une liste
+def get_path_regular(lign):
+    file = open(lign, 'r')
+    content = file.read()
+    path_fromtxt = []
     
+    splitted_content = content.split('\n\n')      #on split le contenu du txt
+    path_fromtxt.append(splitted_content[0])    #on ajoute l'elemnt du txt dans une liste vide
+    
+    
+    string_setup = ''.join(path_fromtxt)    #manipulation de la nouvelle liste en str pour pouvoir la split
+    new_path = string_setup.split(' N ')    #split pour avoir la liste de tout les arret d'une ligne
+    
+    return new_path
+
+
+
+def get_path_holidays(lign):
+    file = open(lign, 'r')
+    content = file.read()
+    path_fromtxt = []
+    
+    splitted_content = content.split('\n\n')      #on split le contenu du txt
+    path_fromtxt.append(splitted_content[3])    #on ajoute l'elemnt du txt dans une liste vide
+    
+    
+    string_setup = ''.join(path_fromtxt)    #manipulation de la nouvelle liste en str pour pouvoir la split
+    new_path = string_setup.split(' N ')    #split pour avoir la liste de tout les arret d'une ligne
+    
+    return new_path
+    
+print(get_path_holidays('1_Poisy-ParcDesGlaisins.txt'))    
+#=================================================
+
+
+#Exemple test pour un bus
+start = 'LYCÉE_DE_POISY'
+end = 'PARC_DES_GLAISINS'
+
+bus = Bus('1',start,end)
+
+
+
+
